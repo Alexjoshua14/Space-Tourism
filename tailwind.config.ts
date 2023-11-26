4afa60ee87c8722682391bc230028515b75d07ac
Alex <ajoshua14@icloud.com>
2023-11-26 13:35:39 -0800

    Begin modularizing design system

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,16 @@ module.exports = {
         "2xl": "1400px",
       },
     },
+    fontSize: {
+      'h1': '150px',
+      'h2': '100px',
+      'h3': '56px',
+      'h4': '32px',
+      'h5': ['28px', { letterSpacing: '4.75px'}],
+      'subheading1':  '28px',
+      'subheading2':  [ '14px', { letterSpacing: '2.7px'}],
+      'navText': [ '16px', { letterSpacing: '2.7px'}],
+    },
     extend: {
       keyframes: {
         "accordion-down": {
@@ -34,11 +44,15 @@ module.exports = {
         'primary': '#FFFFFF',
         'secondary': '#D0D6F9',
       },
+      backgroundColor: {
+        'primary': '#0B0D17'
+      },
       fontFamily: {
         sans: ['var(--font-barlow)'],
         header: ['var(--font-barlow-condensed)'],
         serif: ['var(--font-bellefair)'],
       },
+      
     },
   },
   plugins: [require("tailwindcss-animate")],
